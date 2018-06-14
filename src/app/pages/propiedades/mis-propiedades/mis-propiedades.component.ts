dbde75e43c1b58fd9c2c1bd3692e2fcfc36aab25
Harold <harold@inka-labs.com>
2018-06-14 16:59:31 -0500

    edit and view properties

--- src/app/pages/propiedades/mis-propiedades/mis-propiedades.component.ts
+++ src/app/pages/propiedades/mis-propiedades/mis-propiedades.component.ts
@@ -1,3 +1,41 @@
 /**
  * Created by harold on 6/5/18.
  */
+
+@Component({
+	selector: 'ngx-mis-propiedades',
+  	templateUrl: './mis-propiedades.component.html'
+})
+
+export class MisPropiedadesComponent implements OnInit() {
+
+	private properties;
+
+	settings = {
+    noDataMessage: 'No se encontraron registros.',
+    actions: {
+      add: false,
+      edit: false,
+      delete: false
+    },
+    columns: {
+      id: {
+        title: 'Codigo',
+        type: 'string',
+      },
+      name: {
+        title: 'Nombre',
+        type: 'string',
+      },
+      office_name: {
+        title: 'Oficina',
+        type: 'string',
+      },            
+      user_name: {
+        title: 'Agente',
+        type: 'string'
+      }      
+    },
+  };
+
+}
