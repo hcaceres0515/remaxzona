ea18d8249d143cf99b74f91a87a10420e80e2faa
Harold Caceres <harold.caceres.zea@gmail.com>
2018-06-22 00:34:06 -0500

    dist

--- src/app/@core/config/constanst.ts
+++ src/app/@core/config/constanst.ts
@@ -2,19 +2,19 @@
  * Created by harold on 4/29/18.
  */
 
-
-export const PATHS = {
-
-  SERVER: 'http://localhost/remaxzona/',
-  API: 'http://localhost/remaxzona/index.php?d=api',
-  BASE_URL: 'http://localhost:4200/'
-
-}
-
+//
 // export const PATHS = {
 //
-//   SERVER: 'http://54.233.240.178/remaxzona/',
-//   API: 'http://54.233.240.178/remaxzona/index.php?d=api',
+//   SERVER: 'http://localhost/remaxzona/',
+//   API: 'http://localhost/remaxzona/index.php?d=api',
 //   BASE_URL: 'http://localhost:4200/'
 //
 // }
+
+export const PATHS = {
+
+  SERVER: 'http://54.233.240.178/remaxzona/',
+  API: 'http://54.233.240.178/remaxzona/index.php?d=api',
+  BASE_URL: 'http://localhost:4200/'
+
+}
