fbf4b35927b4d3a688d1e6210de11fb337186653
Harold Caceres <harold.caceres.zea@gmail.com>
2018-06-24 22:07:49 -0500

    CRUD properties

--- src/app/@core/config/constanst.ts
+++ src/app/@core/config/constanst.ts
@@ -3,19 +3,20 @@
  */
 
 
-export const PATHS = {
-
-  SERVER: 'http://localhost/remaxzona/',
-  API: 'http://localhost/remaxzona/index.php?d=api',
-  BASE_URL: 'http://localhost:4200/',
-  MAP_ICON: 'http://localhost/remaxzona/resources_dev/tools/marker_map.png',
-
-}
-
 // export const PATHS = {
 //
-//   SERVER: 'http://54.233.240.178/remaxzona/',
-//   API: 'http://54.233.240.178/remaxzona/index.php?d=api',
-//   BASE_URL: 'http://localhost:4200/'
+//   SERVER: 'http://localhost/remaxzona/',
+//   API: 'http://localhost/remaxzona/index.php?d=api',
+//   BASE_URL: 'http://localhost:4200/',
+//   MAP_ICON: 'http://localhost/remaxzona/resources_dev/tools/marker_map.png',
 //
 // }
+
+export const PATHS = {
+
+  SERVER: 'http://54.233.240.178/remaxzona/',
+  API: 'http://54.233.240.178/remaxzona/index.php?d=api',
+  BASE_URL: 'http://localhost:4200/',
+  MAP_ICON: 'http://54.233.240.178/remaxzona/resources_dev/tools/marker_map.png',
+
+}
