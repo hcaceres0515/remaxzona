1ae4838de81cc83f6cf4a54f84ac8a131ce23cb6
Harold Caceres <harold.caceres.zea@gmail.com>
2018-07-13 07:49:26 -0500

    deploy

--- src/app/@core/config/constanst.ts
+++ src/app/@core/config/constanst.ts
@@ -3,20 +3,20 @@
  */
 
 //
-export const PATHS = {
-
-  SERVER: 'http://localhost/remaxzona/',
-  API: 'http://localhost/remaxzona/index.php?d=api',
-  BASE_URL: 'http://localhost:4200/',
-  MAP_ICON: 'http://localhost/remaxzona/resources_dev/tools/marker_map.png',
-
-}
-
 // export const PATHS = {
 //
-//   SERVER: 'http://54.233.240.178/remaxzona/',
-//   API: 'http://54.233.240.178/remaxzona/index.php?d=api',
-//   BASE_URL: 'http://54.233.240.178/remax/',
-//   MAP_ICON: 'http://54.233.240.178/remaxzona/resources_dev/tools/marker_map.png',
+//   SERVER: 'http://localhost/remaxzona/',
+//   API: 'http://localhost/remaxzona/index.php?d=api',
+//   BASE_URL: 'http://localhost:4200/',
+//   MAP_ICON: 'http://localhost/remaxzona/resources_dev/tools/marker_map.png',
 //
 // }
+
+export const PATHS = {
+
+  SERVER: 'http://54.94.212.147/remaxzona/',
+  API: 'http://54.94.212.147/remaxzona/index.php?d=api',
+  BASE_URL: 'http://54.94.212.147/remax/',
+  MAP_ICON: 'http://54.94.212.147/remaxzona/resources_dev/tools/marker_map.png',
+
+}
